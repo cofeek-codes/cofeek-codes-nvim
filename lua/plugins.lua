889c6d0cb7942c781bc897b381a2bed4485f506d
cofeek-codes <kormyshev11@mail.ru>
2022-08-12 00:33:18 +0500

    packer installed

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,5 +10,9 @@ end
 vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
- use 'wbthomason/packer.nvim',
+ use 'wbthomason/packer.nvim'
+ use {
+  'svrana/neosolarized.nvim',
+  requires = {'tjdevries/colorbuddy.nvim'}
+ }
 end)
