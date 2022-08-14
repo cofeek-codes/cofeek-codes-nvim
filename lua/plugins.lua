217f9cba5efb88cccab9694139abad8b5f246da5
cofeek-codes <kormyshev11@mail.ru>
2022-08-14 17:57:13 +0500

    stable...

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -17,7 +17,4 @@ packer.startup(function(use)
  }
  use 'hoob3rt/lualine.nvim'
  use 'neovim/nvim-lspconfig'
- use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
- use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
- use 'hrsh7th/nvim-cmp' -- Completion
 end)
