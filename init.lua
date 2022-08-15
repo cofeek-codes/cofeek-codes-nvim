fe3527e15f023d0f840576c95843406cd8e94af1
cofeek-codes <kormyshev11@mail.ru>
2022-08-16 00:49:03 +0500

    css server

--- init.lua
+++ init.lua
@@ -6,6 +6,15 @@ local has = function(x)
   return vim.fn.has(x) == 1
 end
 
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+
+
 require 'lspconfig'.pyright.setup {}
 require 'lspconfig'.tsserver.setup {}
 require 'lspconfig'.html.setup{}
+require'lspconfig'.cssls.setup {
+  capabilities = capabilities,
+}
