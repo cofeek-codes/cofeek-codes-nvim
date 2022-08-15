3d7a260d8cb44d2624a295136e009c2284aeb3a8
cofeek-codes <kormyshev11@mail.ru>
2022-08-15 17:35:31 +0500

    lua-language-server

--- after/plugin/lspconfig.rc.lua
+++ after/plugin/lspconfig.rc.lua
@@ -4,11 +4,11 @@ if (not status) then return end
 local protocol = require('vim.lsp.protocol')
 
 local on_attach = function(client,bufnr)
-  if client.server.capabilities.documentFormattingProvider then
-  vim.api.nvim_command [[augroup Format]]
-  vim.api.nvim_command [[autocmd! * <buffer>]]
-  vim.api.nvim_command [[autocmd! BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-  vim.api.nvim_command [[augroup END]]
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_command [[augroup Format]]
+    vim.api.nvim_command [[autocmd! * <buffer>]]
+    vim.api.nvim_command [[autocmd! BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+    vim.api.nvim_command [[augroup END]]
 
 
   end
