8760e5f9a14fafe541fe3f6a187c26d8b789dd82
cofeek-codes <kormyshev11@mail.ru>
2022-08-15 23:37:58 +0500

    formatting is working (partially)

--- after/plugin/lspconfig.rc.lua
+++ after/plugin/lspconfig.rc.lua
@@ -5,12 +5,11 @@ local protocol = require('vim.lsp.protocol')
 
 local on_attach = function(client,bufnr)
   if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_command [[augroup Format]]
-    vim.api.nvim_command [[autocmd! * <buffer>]]
-    vim.api.nvim_command [[autocmd! BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-    vim.api.nvim_command [[augroup END]]
-
-
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("Format", { clear = true }),
+      buffer = bufnr,
+      callback = function() vim.lsp.buf.formatting_seq_sync() end
+    })
   end
 end
 
--- init.lua
+++ init.lua
@@ -6,5 +6,5 @@ local has = function(x)
   return vim.fn.has(x) == 1
 end
 
-require'lspconfig'.pyright.setup{}
-require'lspconfig'.tsserver.setup{}
+require 'lspconfig'.pyright.setup {}
+require 'lspconfig'.tsserver.setup {}
