ac6ee0dfe4992951cefa0891aebcab2cd6c85831
cofeek-codes <kormyshev11@mail.ru>
2023-03-07 15:57:42 +0500

    base analyzer

--- after/plugin/lspconfig.rc.lua
+++ after/plugin/lspconfig.rc.lua
@@ -96,7 +96,13 @@ nvim_lsp.astro.setup {}
 nvim_lsp.tailwindcss.setup {}
 nvim_lsp.omnisharp.setup {}
 nvim_lsp.gopls.setup {}
-nvim_lsp.rust_analyzer.setup {}
+nvim_lsp.rust_analyzer.setup {
+  nvim_lsp.rust_analyzer.setup({
+    on_attach=on_attach,
+
+})
+
+}
 
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
