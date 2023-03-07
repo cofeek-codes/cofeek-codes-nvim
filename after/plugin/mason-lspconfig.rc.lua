eece42feeb4c74c387844b29f6f69541f75cf031
cofeek-codes <kormyshev11@mail.ru>
2023-03-08 01:56:20 +0500

    rust-analyzer added to mason default servers

--- after/plugin/mason-lspconfig.rc.lua
+++ after/plugin/mason-lspconfig.rc.lua
@@ -2,6 +2,6 @@ local status, mason = pcall(require, "mason-lspconfig")
 if (not status) then return end
 
 mason.setup {
-    ensure_installed = { 'tsserver', 'pyright', 'jsonls', 'html', 'cssls', 'intelephense', 'astro', 'tailwindcss', 'omnisharp', 'gopls'},
+    ensure_installed = { 'tsserver', 'pyright', 'jsonls', 'html', 'cssls', 'intelephense', 'astro', 'tailwindcss', 'omnisharp', 'gopls', 'rust_analyzer'},
     automatic_installation = true
 }
