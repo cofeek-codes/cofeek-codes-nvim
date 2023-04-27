5545b1664312f5d8e9cb5becd24bc68a8566e680
cofeek-codes <kormyshev11@mail.ru>
2023-04-27 15:44:20 +0500

    need to fix autoinstall

--- after/plugin/mason-lspconfig.rc.lua
+++ after/plugin/mason-lspconfig.rc.lua
@@ -2,7 +2,7 @@ local status, mason = pcall(require, "mason-lspconfig")
 if (not status) then return end
 
 mason.setup {
+    automatic_installation = false,
     ensure_installed = { 'tsserver', 'pyright', 'jsonls', 'html', 'cssls', 'intelephense', 'astro', 'tailwindcss', 'omnisharp', 'gopls', 'rust_analyzer', 'prismals', 'clangd'},
 -- :MasonInstallAll to install
-    automatic_installation = false
 }
