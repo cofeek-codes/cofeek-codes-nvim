82c3cb47453c94647eae8b3ee2f0297e484d5a5e
cofeek-codes <kormyshev11@mail.ru>
2023-07-18 13:17:29 +0500

    commands got border and icon

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -124,3 +124,74 @@ cmp.setup(
         },
     }
 )
+
+cmp.setup.cmdline(":", {
+    completion = {
+        autocomplete = false,
+    },
+    mapping = cmp.mapping.preset.cmdline({
+        ["<C-k>"] = cmp.mapping({
+            c = function(fallback)
+                if cmp.visible() then
+                    return cmp.select_prev_item()
+                end
+                fallback()
+            end,
+        }),
+        ["<C-j>"] = cmp.mapping({
+            c = function(fallback)
+                if cmp.visible() then
+                    return cmp.select_next_item()
+                end
+                fallback()
+            end,
+        }),
+        ["<Tab>"] = cmp.mapping({
+            c = function()
+                if cmp.visible() then
+                    return cmp.select_next_item()
+                else
+                    cmp.complete()
+                    cmp.select_next_item()
+                    return
+                end
+            end,
+        }),
+        ["<S-Tab>"] = cmp.mapping({
+            c = function()
+                if cmp.visible() then
+                    return cmp.select_prev_item()
+                else
+                    cmp.complete()
+                    cmp.select_next_item()
+                    return
+                end
+            end,
+        }),
+    }),
+    sources = {
+        { name = "path" },
+        {
+            name = "cmdline",
+            option = {
+                ignore_cmds = { "Man", "!" },
+            },
+        },
+    },
+    formatting = {
+        fields = { "abbr", "kind" },
+        format = lspkind.cmp_format({
+            mode = "symbol_text", -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+            maxwidth = 50,        -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            before = function(_, vim_item)
+                if vim_item.kind == "Variable" then
+                    vim_item.kind = ""
+                    return vim_item
+                end
+                -- just show the icon
+                vim_item.kind = lspkind.symbolic(vim_item.kind) and lspkind.symbolic(vim_item.kind) or vim_item.kind
+                return vim_item
+            end,
+        }),
+    },
+})
