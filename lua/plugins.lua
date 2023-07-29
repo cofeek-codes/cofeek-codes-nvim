c8318811bd8da4318d3ebaced73488469b6b1a12
cofeek-codes <kormyshev11@mail.ru>
2023-07-29 18:26:40 +0500

    haskell lsp setup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -163,6 +163,45 @@ local p = {
 			vim.cmd("colorscheme solarized")
 		end
 	},
+	{
+		'mrcjkb/haskell-tools.nvim',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			'nvim-telescope/telescope.nvim', -- optional
+		},
+		ft = "haskell",
+		config = function()
+			local ht = require('haskell-tools')
+			local def_opts = { noremap = true, silent = true, }
+			ht.start_or_attach {
+				hls = {
+					on_attach = function(client, bufnr)
+						local opts = vim.tbl_extend('keep', def_opts, { buffer = bufnr, })
+						-- haskell-language-server relies heavily on codeLenses,
+						-- so auto-refresh (see advanced configuration) is enabled by default
+						vim.keymap.set('n', '<C-h>c', vim.lsp.codelens.run, opts)
+						vim.keymap.set('n', 'K', ht.hoogle.hoogle_signature, opts)
+						vim.keymap.set('n', '<C-h>a', ht.lsp.buf_eval_all, opts)
+					end,
+				},
+			}
+
+			-- Suggested keymaps that do not depend on haskell-language-server:
+			local bufnr = vim.api.nvim_get_current_buf()
+			-- set buffer = bufnr in ftplugin/haskell.lua
+			local opts = { noremap = true, silent = true, buffer = bufnr }
+
+			-- Toggle a GHCi repl for the current package
+			vim.keymap.set('n', '<C-h>rf', ht.repl.toggle, opts)
+			-- Toggle a GHCi repl for the current buffer
+			vim.keymap.set('n', '<C-h>rr', function()
+				ht.repl.toggle(vim.api.nvim_buf_get_name(0))
+			end, def_opts)
+			vim.keymap.set('n', '<C-h>rq', ht.repl.quit, opts)
+			-- dap.discover_configurations(bufnr)
+		end,
+		branch = '1.x.x', -- recommended
+	}
 }
 
 
